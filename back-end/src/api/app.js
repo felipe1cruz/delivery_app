7a37e0f56cbdbc2c20d0eaf9dfbb7fc052a1dd76
Gusvioli <gustavovieiradeoliveira@gmail.com>
2023-01-24 14:25:57 -0300

    Reqs 11 ate 16 em andamento e instalação do CORS

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const loginRouter = require('../routes/loginRouter');
 const customerProducts = require('../routes/customerProductsRouter');
 const errorMiddleware = require('../middlewares/errorMiddleware');
+const cors = require('cors');
+
+
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(loginRouter);
 app.use(customerProducts);
--- front-end/src/components/Products.jsx
+++ front-end/src/components/Products.jsx
@@ -1,17 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import { requestData } from '../services/requests';
+import React, { useEffect, useState } from "react";
+import { requestData } from "../services/requests";
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [quantity, setQuantity] = useState([{ id: 0, valor: 0 }]);
+
+  const dataTests = (productId) => {
+    const cardPrince = `customer_products__element-card-price-${productId}`;
+    const cardTitle = `customer_products__element-card-title-${productId}`;
+    const cardBgImage = `customer_products__img-card-bg-image-${productId}`;
+    const cardRmItem = `customer_products__button-card-rm-item-${productId}`;
+    const cardQuantity = `customer_products__input-card-quantity-${productId}`;
+    const cardAddItem = `customer_products__button-card-add-item-${productId}`;
+    return {
+      cardPrince,
+      cardTitle,
+      cardBgImage,
+      cardRmItem,
+      cardQuantity,
+      cardAddItem,
+    };
+  };
+  
+  const addRmQuantity = ({ value }, id) => {
+    switch (value) {
+      case '+':
+        setQuantity([...quantity, { id, valor: quantity.valor + 1 }])
+        break;
+      case '-':
+        setQuantity([...quantity, { id, valor: quantity.valor - 1 }])
+        break 
+    }
+  }
 
   useEffect(() => {
-    requestData('/customer/products')
+    requestData("/customer/products")
       .then((response) => setProducts(response))
-      .catch((e) => console.log(e));
+      .catch();
   }, [products]);
 
   return (
-    <div>{products}</div>
+    <div>
+      {products.map((product) => (
+        <div
+          key={product.id}
+          style={{ border: "solid 1px #ccc", margin: "5px", padding: "5px" }}
+        >
+          <div data-testid={dataTests(product.id).cardPrince}>{product.price}</div>
+          <div data-testid={dataTests(product.id).cardTitle}>{product.name}</div>
+          <img
+            src={product.urlImage}
+            data-testid={dataTests(product.id).cardBgImage}
+          />
+          <button
+          type="button"
+          data-testid={dataTests(product.id).cardRmItem}
+          value="-"
+          onClick={ (e) => addRmQuantity(e.target) }
+          >
+            -
+          </button>
+          <input
+            type="text"
+            data-testid={dataTests(product.id).cardQuantity}
+            value='0'
+          />
+          <button
+          type="button"
+          data-testid={dataTests(product.id).cardAddItem}
+          value="+"
+          onClick={ (e) => addRmQuantity(e.target, product.id) }
+          >
+            +
+          </button>
+        </div>
+      ))}
+    </div>
   );
 }
 

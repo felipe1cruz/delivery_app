fd28cc8811b2cbfcf881cf9b85757d334fc226b9
Gabriel Almeida de Oliveira <eng.gabriel.ao@gmail.com>
2023-01-24 20:53:15 -0300

    Testes para os requisitos 1 e 2, Tela de Login

---
+++ front-end/src/tests/unit/telaLogin.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import App from '../../App';
+import renderWithRouter from '../helpers/renderWithRouter';
+
+describe('Avalia o funcionamento da tela de Login', () => {
+  it('Os elementos básicos estão sendo exibidos?', () => {
+    render(<App />);
+
+    const email = screen.getByRole('textbox', { name: /login/i });
+    const senha = screen.getByLabelText(/senha/i);
+    const botaoLogin = screen.getByRole('button', { name: /login/i });
+    const botaoCadastro = screen.getByRole('button', { name: /ainda não tenho conta/i });
+
+    expect(email).not.toBeInTheDocument();
+    expect(senha).not.toBeInTheDocument();
+    expect(botaoLogin).not.toBeInTheDocument();
+    expect(botaoCadastro).not.toBeInTheDocument();
+  });
+
+  it('Clicar no botão de cadastro leva para a página de cadastro?', () => {
+    const { history } = renderWithRouter(<App />);
+    const botaoCadastro = screen.getByRole('button', { name: /ainda não tenho conta/i });
+
+    userEvent.click(botaoCadastro);
+    let page = history.location.pathname;
+    expect(page).equal('/register');
+
+    history.push('/');
+    page = history.location.pathname;
+    expec(page).equal('/login');
+
+    userEvent.click(botaoCadastro);
+    page = history.location.pathname;
+    expect(page).equal('/register');
+  });
+});

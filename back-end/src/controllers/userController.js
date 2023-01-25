3eeb61b1f169a6df26ca3b6f3bf29ee2c596b7af
felipe-cruz <piradous@gmail.com>
2023-01-25 16:24:02 -0300

    feat: req 04

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -6,6 +6,7 @@ const user = async (req, res, next) => {
     const authenticate = await userService.authenticate(email, password);
     return res.send(authenticate);
   } catch (error) {
+    // return res.status(404).json({ message: 'usuário não encontrado!'});
     next(error);
   }
 };

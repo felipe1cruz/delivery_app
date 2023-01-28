622cf94d146861615975712f752e710bfef77f3c
Gabriel Almeida de Oliveira <eng.gabriel.ao@gmail.com>
2023-01-27 22:04:06 -0300

    Refatorando userService.authenticate: Adiciona Validação de regex no email do login; Simplifica abstração da resposta do model para atender a testes.

--- back-end/src/services/userService.js
+++ back-end/src/services/userService.js
@@ -4,8 +4,10 @@ const errorGenerate = require('../utils/genericErrorHandler');
 const { User } = require('../database/models');
 const { generateToken } = require('../utils/JWT');
 
+const emailRegex = /^[a-z0-9-_.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+
 const userSchema = Joi.object({
-  email: Joi.string().required(),
+  email: Joi.string().pattern(emailRegex).required(),
   password: Joi.string().required().min(6),
 });
 
@@ -14,19 +16,16 @@ const authenticate = async (userEmail, userPassword) => {
   const { error } = userSchema.validate({ email: userEmail, password: userPassword });
   if (error) throw errorGenerate(400, 'Some required fields are missing');
 
-  const { dataValues: user } = await User.findOne({
+  const user = await User.findOne({
     attributes: ['name', 'email', 'role'],
     where: { email: userEmail, password: cryptoPassword },
   });
 
   if (!user) throw errorGenerate(404, 'Not found');
 
-  const token = generateToken(user);
-  return { name: user.name,
-    email: user.email,
-    role: user.role,
-    token,
-   };
+  const token = generateToken(user.dataValues);
+  const { name, email, role } = user.dataValues;
+  return { name, email, role, token };
 };
 
 module.exports = {

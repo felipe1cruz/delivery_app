b4becaeac55ff4aa62fb6e60b777c6e6f31cb127
Gusvioli <gustavovieiradeoliveira@gmail.com>
2023-01-29 23:26:28 -0300

    Fix erro Lint

--- back-end/src/controllers/customerOrdersController.js
+++ back-end/src/controllers/customerOrdersController.js
@@ -5,11 +5,11 @@ const sales = async (_req, res) => {
   return res.status(200).json(allSales);
 };
 
-const salesId = async (req, res) => {
-  const { id } = req.params;
-  const allSales = await customerOrdersService.getSalesId(id);
-  return res.status(200).json(allSales);
-};
+// const salesId = async (req, res) => {
+//   const { id } = req.params;
+//   const allSales = await customerOrdersService.getSalesId(id);
+//   return res.status(200).json(allSales);
+// };
 
 module.exports = {
   sales,

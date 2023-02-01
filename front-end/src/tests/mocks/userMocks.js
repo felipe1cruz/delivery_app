7face53b581b4555c002d1cf1c86869487f25213
Gabriel Almeida de Oliveira <eng.gabriel.ao@gmail.com>
2023-02-01 17:51:09 -0300

    Novos mocks adicionados e ... Fix: Mocks renomeados para mais clareza

---
+++ front-end/src/tests/mocks/userMocks.js
@@ -0,0 +1,43 @@
+const validUser = {
+  nome: 'Joselito Hermes',
+  senha: 'Sem Noção',
+  email: 'candidato@semnocao.com',
+};
+
+const invalidUser = {
+  nome: 'Renato',
+  senha: '1234',
+  email: 'vi@rus@sem.no.cao.com',
+};
+
+const customerUser = {
+  nome: 'Cliente Zé Birita',
+  senha: '$#zebirita#$',
+  email: 'zebirita@email.com',
+  role: 'customer',
+  senhaCriptografada: '1c37466c159755ce1fa181bd247cb925',
+};
+
+const adminUser = {
+  nome: 'Delivery App Admin',
+  senha: '--adm2@21!!--',
+  email: 'adm@deliveryapp.com',
+  role: 'administrator',
+  senhaCriptografada: 'a4c86edecc5aee06eff8fdeda69e0d04',
+};
+
+const sellerUser = {
+  nome: 'Fulana Pereira',
+  senha: 'fulana@123',
+  email: 'fulana@deliveryapp.com',
+  role: 'seller',
+  senhaCriptografada: '3c28d2b0881bf46457a853e0b07531c6',
+};
+
+export {
+  validUser,
+  invalidUser,
+  customerUser,
+  adminUser,
+  sellerUser,
+};

15e4d1e1c270cbbecca460240c8eed2b86581712
Gabriel Almeida de Oliveira <eng.gabriel.ao@gmail.com>
2023-02-02 01:10:21 -0300

    Novos testes para o front da tela de registro ... Cobrindo 100% neste momento

--- front-end/src/tests/unit/telaRegister.test.js
+++ front-end/src/tests/unit/telaRegister.test.js
@@ -1,47 +1,50 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import App from '../../App';
-import { validUser, invalidUser } from '../mocks/registerMocks';
+import renderWRP from '../helpers/renderWithRouterAndProvider';
+import { validUser, invalidUser } from '../mocks/userMocks';
+
+function getElementsOfInterest() {
+  const nome = screen.getByRole('textbox', { name: /nome/i });
+  const email = screen.getByRole('textbox', { name: /email/i });
+  const senha = screen.getByLabelText(/senha/i);
+  const botaoCadastro = screen.getByRole('button', { name: /cadastrar/i });
+  const msgJaCadastrado = screen.getByText(/usuário já cadastrado!/i, { hidden: true });
+  return { nome, email, senha, botaoCadastro, msgJaCadastrado };
+}
 
 describe('Avalia a Tela de Register', () => {
+  beforeEach(() => {
+    localStorage.clear();
+    const { history } = renderWRP(<App />);
+    history.push('/register');
+  });
+
   it('1 - Os elementos básicos estão sendo exibidos?', () => {
-    render(<App />);
-
-    const nome = screen.getByRole('textbox', { name: /nome/i });
-    const email = screen.getByRole('textbox', { name: /email/i });
-    const senha = screen.getByLabelText(/senha/i);
-    const botaoCadastro = screen.getByRole('button', { name: /cadastrar/i });
-
+    const { nome, email, senha,
+      botaoCadastro, msgJaCadastrado } = getElementsOfInterest();
     expect(nome).toBeInTheDocument();
     expect(email).toBeInTheDocument();
     expect(senha).toBeInTheDocument();
-    expect(botaoCadastro).not.toBeInTheDocument();
+    expect(botaoCadastro).toBeInTheDocument();
+    expect(msgJaCadastrado).toBeInTheDocument();
+    expect(msgJaCadastrado).not.toBeVisible();
   });
 
   it('2 - Botão cadastro é liberado com dados válidos?', () => {
-    render(<App />);
-
-    const nome = screen.getByRole('textbox', { name: /nome/i });
-    const senha = screen.getByLabelText(/senha/i);
-    const email = screen.getByRole('textbox', { name: /email/i });
-    const botaoCadastro = screen.getByRole('button', { name: /cadastrar/i });
+    const { nome, email, senha, botaoCadastro } = getElementsOfInterest();
     expect(botaoCadastro).toBeDisabled();
 
     userEvent.type(nome, validUser.nome);
     userEvent.type(senha, validUser.senha);
     userEvent.type(email, validUser.email);
-    expect(botaoCadastro).not.toBeDisabled();
+    expect(botaoCadastro).toBeEnabled();
   });
 
   it('3 - Nome inválido bloqueia cadastro?', () => {
-    render(<App />);
-
-    const nome = screen.getByRole('textbox', { name: /nome/i });
-    const senha = screen.getByLabelText(/senha/i);
-    const email = screen.getByRole('textbox', { name: /email/i });
-    const botaoCadastro = screen.getByRole('button', { name: /cadastrar/i });
+    const { nome, email, senha, botaoCadastro } = getElementsOfInterest();
     expect(botaoCadastro).toBeDisabled();
 
     userEvent.type(nome, invalidUser.nome);
@@ -51,12 +54,7 @@ describe('Avalia a Tela de Register', () => {
   });
 
   it('4 - Senha inválida bloqueia cadastro?', () => {
-    render(<App />);
-
-    const nome = screen.getByRole('textbox', { name: /nome/i });
-    const senha = screen.getByLabelText(/senha/i);
-    const email = screen.getByRole('textbox', { name: /email/i });
-    const botaoCadastro = screen.getByRole('button', { name: /cadastrar/i });
+    const { nome, email, senha, botaoCadastro } = getElementsOfInterest();
     expect(botaoCadastro).toBeDisabled();
 
     userEvent.type(nome, validUser.nome);
@@ -66,17 +64,35 @@ describe('Avalia a Tela de Register', () => {
   });
 
   it('5 - Email inválido bloqueia cadastro?', () => {
-    render(<App />);
-
-    const nome = screen.getByRole('textbox', { name: /nome/i });
-    const senha = screen.getByLabelText(/senha/i);
-    const email = screen.getByRole('textbox', { name: /email/i });
-    const botaoCadastro = screen.getByRole('button', { name: /cadastrar/i });
+    const { nome, email, senha, botaoCadastro } = getElementsOfInterest();
     expect(botaoCadastro).toBeDisabled();
 
     userEvent.type(nome, validUser.nome);
     userEvent.type(senha, validUser.senha);
     userEvent.type(email, invalidUser.email);
     expect(botaoCadastro).toBeDisabled();
+  });
+
+  it('6 - Conclui cadastro com dados válidos?', async () => {
+    const { nome, email, senha, botaoCadastro } = getElementsOfInterest();
+
+    userEvent.type(nome, validUser.nome);
+    userEvent.type(senha, validUser.senha);
+    userEvent.type(email, validUser.email);
+    expect(botaoCadastro).toBeEnabled();
+    userEvent.click(botaoCadastro);
+    await waitFor(() => expect(botaoCadastro).not.toBeInTheDocument());
+  });
+
+  it('7 - Lança mensagem de erro quando os dados já estão cadastrados?', async () => {
+    const { nome, email, senha,
+      botaoCadastro, msgJaCadastrado } = getElementsOfInterest();
+
+    userEvent.type(nome, validUser.nome);
+    userEvent.type(senha, validUser.senha);
+    userEvent.type(email, validUser.email);
+    expect(botaoCadastro).toBeEnabled();
+    userEvent.click(botaoCadastro);
+    await waitFor(() => expect(msgJaCadastrado).toBeVisible());
   });
 });
